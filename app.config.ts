3097a75c5017c4111412688a7321108d5bbc651d
gronxb <gron1gh1@gmail.com>
2025-09-13 16:47:32 +0900

    fix: something [force-build]

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default {
 	expo: {
 		name: 'my-app',
 		slug: 'my-app',
-		version: '1.0.703',
+		version: '1.0.704',
 		orientation: 'portrait',
 		icon: './src/assets/images/icon.png',
 		scheme: 'gymapp',
