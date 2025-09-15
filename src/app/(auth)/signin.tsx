f519b5854498d7b6675fa8088aab2576463ab4b3
gronxb <gron1gh1@gmail.com>
2025-09-15 09:36:04 +0900

    fix: force update trigeer and v4

--- src/app/(auth)/signin.tsx
+++ src/app/(auth)/signin.tsx
@@ -33,7 +33,7 @@ const Signin: React.FC = (): React.JSX.Element => {
 				showsVerticalScrollIndicator={false}>
 				{/* Header */}
 				<View style={styles.header}>
-					<Text style={styles.title}>HI OTA v3</Text>
+					<Text style={styles.title}>HI OTA v4</Text>
 					<Text style={styles.subtitle}>Enter your login information</Text>
 					<Text
 						style={styles.subtitle}
